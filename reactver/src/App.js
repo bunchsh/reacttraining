4a2c818881b0913dff54b3445820b7234be38db6
bunchsh <onee.ssong@gmail.com>
2022-02-10 18:01:28 +0900

    GlobalStyle.js 로 전역 스타일 컴포넌트 추가

--- reactver/src/App.js
+++ reactver/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import {Route, Link, Routes} from 'react-router-dom';
 
+import GlobalStyle from "../src/assets/css/GlobalStyle"
+
 import ComponentHeader from './fixed/ComponentHeader';
 import ComponentFooter from './fixed/ComponentFooter';
 
 const App = () => {
   return (
     <div>
+      <GlobalStyle />
       <div className="header"><ComponentHeader/></div>
       <div className="footer"><ComponentFooter/></div>
       
