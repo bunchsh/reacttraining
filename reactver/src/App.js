66b40ddfae1350da1681d097e90b5d71bb7abff8
ysw7939 <91578199+ysw7939@users.noreply.github.com>
2022-02-10 19:00:46 +0900

    main

--- reactver/src/App.js
+++ reactver/src/App.js
@@ -3,21 +3,24 @@ import React from 'react';
 import {Route, Link, Routes} from 'react-router-dom';
 
 import GlobalStyle from "../src/assets/css/GlobalStyle"
+import indexStyle from "../src/assets/css/index.module.css"
 
 import ComponentHeader from './fixed/ComponentHeader';
 import ComponentFooter from './fixed/ComponentFooter';
+import Main from './fixed/main';
 
 const App = () => {
   return (
     <div>
       <GlobalStyle />
       <div className="header"><ComponentHeader/></div>
-      <div className="footer"><ComponentFooter/></div>
       
-    <Routes>
-      <Route element={<ComponentHeader/>}/>
-      <Route element={<ComponentFooter/>}/>
-    </Routes>
+      <Routes>
+      <Route path="/main" element={<Main/>}/>
+      </Routes>
+      
+      <div className="footer"><ComponentFooter/></div>
+
     </div>
   );
 }
---
+++ reactver/src/fixed/main.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import {Route, Link, Routes} from 'react-router-dom';
+
+import GlobalStyle from "../assets/css/GlobalStyle"
+import indexStyle from "../assets/css/index.module.css"
+
+
+const Main = () => {
+  return (
+    <div>
+        <GlobalStyle />
+        <div ClassName={indexStyle.content}>
+            <h2 className={indexStyle.contentH2}>편안한 보금자리를 약속드립니다. 한강빌리지</h2>
+            <ul className={indexStyle.contentUl}>
+                <li className={indexStyle.contentLi}>
+                    <a href="/main" className={`${indexStyle.contentA} ${indexStyle.contentIcon1}`}>분양안내</a>
+                </li>
+                <li className={indexStyle.contentLi}>
+                    <Link to="" className={`${indexStyle.contentA} ${indexStyle.contentIcon2}`}>프리미엄</Link>
+                </li>
+                <li className={indexStyle.contentLi}>
+                    <Link to="" className={`${indexStyle.contentA} ${indexStyle.contentIcon3}`}>단지안내</Link>
+                </li>
+                <li className={indexStyle.contentLi}>
+                    <Link to="" className={`${indexStyle.contentA} ${indexStyle.contentIcon4}`}>인테리어</Link>
+                </li>
+            </ul>
+        </div>
+    </div>
+  );
+}
+
+export default Main;
