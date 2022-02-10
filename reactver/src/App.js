3e8be28b7b142ff2f3a39d4e651247362bcecb8a
ysw7939 <91578199+ysw7939@users.noreply.github.com>
2022-02-10 17:38:23 +0900

    header

--- reactver/src/App.js
+++ reactver/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 import {Route, Link, Routes} from 'react-router-dom';
 
+import ComponentHeader from './fixed/ComponentHeader';
 import ComponentFooter from './fixed/ComponentFooter';
 
 const App = () => {
   return (
     <div>
+      <h1><ComponentHeader/></h1>
+      <h1><ComponentFooter/></h1>
       
     <Routes>
+      <Route path='/header' element={<ComponentHeader/>}/>
       <Route path='/footer' element={<ComponentFooter/>}/>
     </Routes>
     </div>
---
+++ reactver/src/fixed/ComponentHeader.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+import logo from '../assets/img/logo.png'
+import home from '../assets/img/home.jpg'
+
+import HeaderStyle from '../assets/css/header.module.css';
+
+const ComponentHeader = () => {
+    return (
+        <div className={HeaderStyle.header}>
+            <h1 className={HeaderStyle.headerH1}>
+                <Link to="">
+                    <img src={logo} alt="한강빌리지"/>
+                </Link>
+            </h1>
+            <span className={HeaderStyle.headerSpan}>
+                <Link to="">
+                    <img src={home} alt="Home"/>
+                </Link>
+            </span>
+        </div>
+    );
+};
+
+export default ComponentHeader
