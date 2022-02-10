fd0a7cbd9a527d209b6cd27a392a73b193327b2d
bunchsh <onee.ssong@gmail.com>
2022-02-10 20:15:16 +0900

    infoSub2.js 수정

--- reactver/src/pages/Info2.js
+++ reactver/src/pages/Info2.js
@@ -4,18 +4,19 @@ import {Link} from "react-router-dom";
 import GlobalStyle from "../assets/css/GlobalStyle"
 import InfoSub2 from '../sub/InfoSub2';
 
-import Info2Style from "../assets/css/tab.module.css"
+import Info2Style from "../assets/css/Info2.module.css"
+import InfoTab from "../assets/css/tab.module.css"
 
 const Info2 = () => {
   return (
     <div>
       <GlobalStyle />
-      <ul className={Info2Style.tabUl}>
-          <li className={Info2Style.tabLi}>
-              <Link className={Info2Style.tabLink} to="/info1/infosub1">분양안내</Link>
+      <ul className={InfoTab.tabUl}>
+          <li className={InfoTab.tabLi}>
+              <Link className={InfoTab.tabLink} to="/info1/infosub1">분양안내</Link>
           </li>
-          <li className={Info2Style.tabLi}>
-              <Link className={Info2Style.tabLink} to="/info2/infosub2">단지안내</Link>
+          <li className={InfoTab.tabLi}>
+              <Link className={InfoTab.tabLink} to="/info2/infosub2">단지안내</Link>
           </li>
       </ul>
       <InfoSub2/>
--- reactver/src/sub/InfoSub2.js
+++ reactver/src/sub/InfoSub2.js
@@ -15,30 +15,30 @@ import img7 from '../assets/img/pic_ca1_img07.jpg'
 const InfoSub2 = () => {
     return (
         <div className={InfoSub2Style.content}>
-        <GlobalStyle />
-        <h2 className={InfoSub2Style.contentH2}>친환경 주거단지, 사람도 자연도 행복합니다.</h2>
-        <ul>
-            <li className={InfoSub2Style.contentLi}>
-                <Link className={InfoSub2Style.contentLink} to=""><img className={InfoSub2Style.contentImg} src={img2} alt="조감도1" /></Link>
-            </li>
-            <li className={InfoSub2Style.contentLi}>
-                <Link className={InfoSub2Style.contentLink} to=""><img className={InfoSub2Style.contentImg} src={img3}  alt="조감도2" /></Link>
-            </li>
-            <li className={InfoSub2Style.contentLi}>
-                <Link className={InfoSub2Style.contentLink} to=""><img className={InfoSub2Style.contentImg} src={img4} alt="조감도3" /></Link>
-            </li>
-            <li className={InfoSub2Style.contentLi}>
-                <Link className={InfoSub2Style.contentLink} to=""><img className={InfoSub2Style.contentImg} src={img5} alt="조감도4" /></Link>
-            </li>
-            <li className={InfoSub2Style.contentLi}>
-                <Link className={InfoSub2Style.contentLink} to=""><img className={InfoSub2Style.contentImg} src={img6} alt="조감도5" /></Link>
-            </li>
-            <li className={InfoSub2Style.contentLi}>
-                <Link className={InfoSub2Style.contentLink} to=""><img className={InfoSub2Style.contentImg} src={img7} alt="조감도6" /></Link>
-            </li>
-        </ul>
-    </div>
-  );
+            <GlobalStyle />
+            <h2 className={InfoSub2Style.contentH2}>친환경 주거단지, 사람도 자연도 행복합니다.</h2>
+            <ul className={InfoSub2Style.contentUl}>
+                <li className={InfoSub2Style.contentLi}>
+                    <Link className={InfoSub2Style.contentLink} to=""><img className={InfoSub2Style.contentImg} src={img2} alt="조감도1" /></Link>
+                </li>
+                <li className={InfoSub2Style.contentLi}>
+                    <Link className={InfoSub2Style.contentLink} to=""><img className={InfoSub2Style.contentImg} src={img3}  alt="조감도2" /></Link>
+                </li>
+                <li className={InfoSub2Style.contentLi}>
+                    <Link className={InfoSub2Style.contentLink} to=""><img className={InfoSub2Style.contentImg} src={img4} alt="조감도3" /></Link>
+                </li>
+                <li className={InfoSub2Style.contentLi}>
+                    <Link className={InfoSub2Style.contentLink} to=""><img className={InfoSub2Style.contentImg} src={img5} alt="조감도4" /></Link>
+                </li>
+                <li className={InfoSub2Style.contentLi}>
+                    <Link className={InfoSub2Style.contentLink} to=""><img className={InfoSub2Style.contentImg} src={img6} alt="조감도5" /></Link>
+                </li>
+                <li className={InfoSub2Style.contentLi}>
+                    <Link className={InfoSub2Style.contentLink} to=""><img className={InfoSub2Style.contentImg} src={img7} alt="조감도6" /></Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default InfoSub2;
