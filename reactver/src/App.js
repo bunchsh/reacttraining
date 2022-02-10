f097770f8923130413ad0d3701afca88f1c71032
bunchsh <onee.ssong@gmail.com>
2022-02-10 16:24:25 +0900

    ComponentFooter 구현

--- reactver/src/App.js
+++ reactver/src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import {Route, Link, Routes} from 'react-router-dom';
 
+import ComponentFooter from './fixed/ComponentFooter';
+
 const App = () => {
   return (
     <div>
       
+    <Routes>
+      <Route path='/footer' element={<ComponentFooter/>}/>
+    </Routes>
     </div>
   );
 }
--- reactver/src/fixed/componentFooter.js
+++ reactver/src/fixed/componentFooter.js
@@ -1,9 +1,27 @@
 import React from 'react';
+// import {Route, Link, Routes} from "react-router-dom";
+
+import call from '../assets/img/qlink_call.gif'
+import map from '../assets/img/qlink_map.gif'
+
+import footerStyle from '../assets/css/footer.module.css';
 
 const ComponentFooter = () => {
   return (
-    <div className="footer">
-        
+    <div className={footerStyle.footer}>
+        <ul className={footerStyle.footerUl}>
+            <li className={footerStyle.footerLi}>
+                <a href="tel:12345678">
+                    <img src={call} alt="분양상담전화" /></a>
+            </li>
+            <li className={footerStyle.footerLi}>
+                <a href="map">
+                <img src={map} alt="찾아오시는 길" /></a>
+            </li>
+        </ul>
+        <address className={footerStyle.footerAddress}>
+            Copyright 2014. HANGANG VILLAGE
+        </address>
     </div>
   );
 }
