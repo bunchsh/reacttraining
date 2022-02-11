34092dd0cca054b155f885169e45d65e6b98b8f5
ysw7939 <91578199+ysw7939@users.noreply.github.com>
2022-02-11 16:21:06 +0900

    indexItem num수정

--- reactver/src/components/IndexItem.js
+++ reactver/src/components/IndexItem.js
@@ -7,13 +7,10 @@ import indexStyle from "../assets/css/index.module.css"
 
 
 const IndexItem = ({num, link, msg}) => {
-
-    let ClassArray = [indexStyle.contentA, {num}.num]
-    console.log(ClassArray);
-
+    
     return (
             <li className={indexStyle.contentLi}>
-                <Link to={link} className={ClassArray.join(' ')}>{msg}</Link>
+                <Link to={link} className={[indexStyle.contentA, {num}.num].join(' ')}>{msg}</Link>
             </li>
     );
 };
